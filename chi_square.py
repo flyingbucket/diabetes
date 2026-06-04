80e965e8ae80a70e3f177be3404120d5313366c6
flyingbucket <menglinli598@gmail.com>
2026-06-04 19:22:16 +0800

    fix chi2

--- chi_square.py
+++ chi_square.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from scipy.stats import chi2_contingency
 
-df = pd.read_csv("outcome_new.csv")
+df = pd.read_csv("outcome.csv")
 
 target_cols = ["Readmission", "Initial dialysis", "MACE"]
 
